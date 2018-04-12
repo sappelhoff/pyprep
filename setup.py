5d4aaad2fa9c95289a3c81cf7b65087266774567
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-12 14:25:13 +0200

    some package organization

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup pyprep."""
+from setuptools import setup
+
+setup(name='pyprep',
+      version='0.0.1',
+      description='A python implementation of the Preprocessing Pipeline'
+                  ' (PREP) for EEG data ',
+      url='http://github.com/sappelhoff/pyprep',
+      author='Stefan Appelhoff',
+      author_email='stefan.appelhoff@mailbox.org',
+      license='MIT',
+      packages=['pyprep'],
+      zip_safe=False)
