b905d63a7a75f3c4991590e6e81ebb861566188b
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-23 16:46:49 +0200

    add test for ransac too few preds

--- test/test_noisy.py
+++ test/test_noisy.py
@@ -1,21 +1,43 @@
 """Test the noisy module."""
 
+from nose.tools import assert_raises
+
 import numpy as np
 import mne
 
 from pyprep.noisy import Noisydata
 
 
-def test_init():
+# Make a random MNE object
+sfreq = 1000.
+t = np.arange(0, 600, 1./sfreq)  # 10 minutes recording
+signal_len = t.shape[0]
+n_chans = 3
+ch_names = ['Cz', 'Pz', 'Oz']
+
+# Make a random signal
+signal = np.zeros((n_chans, signal_len))
+for chan in range(n_chans):
+    # Each channel signal is a sum of random sine waves
+    for freq_i in range(10):
+        freq = np.random.randint(0, 100, signal_len)
+        signal[chan, :] += np.sin(2*np.pi*t*freq)
+
+signal *= 1e-6  # scale to Volts
+
+# Make mne object
+info = mne.create_info(ch_names=ch_names, sfreq=sfreq,
+                       ch_types=['eeg']*n_chans)
+raw = mne.io.RawArray(signal, info)
+
+
+def test_init(raw=raw):
     """Test the class initialization."""
-    # Make a random raw mne object
-    sfreq = 1000.
-    t = np.arange(0, 10, 1./sfreq)
-    n_chans = 3
-    ch_names = ['Cz', 'Pz', 'Oz']
-    X = np.random.random((n_chans, t.shape[0]))
-    info = mne.create_info(ch_names=ch_names, sfreq=sfreq,
-                           ch_types=['eeg']*n_chans)
-    raw = mne.io.RawArray(X, info)
     nd = Noisydata(raw)
     assert nd
+
+
+def test_ransac_too_few_preds(raw=raw):
+    """Test that ransac throws an arror for few predictors."""
+    nd = Noisydata(raw)
+    assert_raises(IOError, nd.find_bad_by_ransac)
