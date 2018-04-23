162f1040f998348ccbf342ab7261de061acf0da3
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-23 17:21:44 +0200

    more tests

--- test/test_noisy.py
+++ test/test_noisy.py
@@ -12,8 +12,8 @@ from pyprep.noisy import Noisydata
 sfreq = 1000.
 t = np.arange(0, 600, 1./sfreq)  # 10 minutes recording
 signal_len = t.shape[0]
-n_chans = 3
-ch_names = ['Cz', 'Pz', 'Oz']
+n_chans = 4
+ch_names = ['Fz', 'Cz', 'Pz', 'Oz']
 
 # Make a random signal
 signal = np.zeros((n_chans, signal_len))
@@ -39,5 +39,20 @@ def test_init(raw=raw):
 
 def test_ransac_too_few_preds(raw=raw):
     """Test that ransac throws an arror for few predictors."""
+    chns = np.random.choice(raw.ch_names, size=3, replace=False)
+    raw.pick_channels(chns)
     nd = Noisydata(raw)
     assert_raises(IOError, nd.find_bad_by_ransac)
+
+
+def test_ransac_too_little_ram(raw=raw):
+    """Test that ransac throws a memory error if not enough available."""
+    nd = Noisydata(raw)
+    chn_pos = nd.chn_pos
+    chn_pos_good = chn_pos
+    good_chn_labs = raw.ch_names
+    n_pred_chns = 4
+    data = raw._data
+    n_samples = 99e99
+    assert_raises(MemoryError, nd._run_ransac, chn_pos, chn_pos_good,
+                  good_chn_labs, n_pred_chns, data, n_samples)
