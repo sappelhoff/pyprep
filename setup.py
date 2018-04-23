fd0bd22d4046263c8f297db9c61717fadd30b1d4
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-23 14:11:52 +0200

    extend setup

--- setup.py
+++ setup.py
@@ -1,13 +1,38 @@
 """Setup pyprep."""
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path
+import io
+
+here = path.abspath(path.dirname(__file__))
+
+# Get long description from README file
+with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='pyprep',
-      version='0.0.1',
-      description='A python implementation of the Preprocessing Pipeline'
-                  ' (PREP) for EEG data ',
+      version='0.1.0',
+      description=('A Python implementation of the preprocessing pipeline'
+                   ' (PREP) for EEG data.'),
+      long_description=long_description,
       url='http://github.com/sappelhoff/pyprep',
       author='Stefan Appelhoff',
       author_email='stefan.appelhoff@mailbox.org',
       license='MIT',
-      packages=['pyprep'],
-      zip_safe=False)
+      classifiers=[
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research'
+      ],
+      keywords='EEG artifact preprocessing data',
+      packages=find_packages(),
+      install_requires=['numpy>=1.14.1', 'scipy>=1.0.0', 'statsmodels>=0.8.0',
+                        'mne>=0.15.0'],
+      python_requires='>=2.7',
+      extras_require={
+        'test': ['nose>=1.3.7']
+      },
+      project_urls={
+        'Bug Reports': 'https://github.com/sappelhoff/pyprep/issues',
+        'Source': 'https://github.com/sappelhoff/pyprep'
+      })
