7be0d0af00a662e15f4c5a12be70a5065229a4e0
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-02-02 13:11:57 +0100

    bump version to dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018, Stefan Appelhoff'
 author = u'Stefan Appelhoff'
 
 # The short X.Y version
-version = u'0.2.3'
+version = u'0.2.3-dev'
 # The full version, including alpha/beta/rc tags
-release = u'0.2.3'
+release = u'0.2.3-dev'
 
 
 # -- General configuration ---------------------------------------------------
