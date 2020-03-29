2a7b0583708be6cf30218030f68f35909d54d47c
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-03-29 16:19:26 +0200

    specify master_doc for RTD [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,7 @@ release = version
 
 # -- General configuration ---------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
