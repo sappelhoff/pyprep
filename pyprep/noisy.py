4fafcd380390c8b439217f0795b974f1e465254a
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-04-04 15:15:01 +0200

    add _find_outliers as private func to noisy

--- pyprep/noisy.py
+++ pyprep/noisy.py
@@ -4,10 +4,24 @@ from distutils.version import LooseVersion
 import mne
 import numpy as np
 from mne.channels.interpolation import _make_interpolation_matrix
-from mne.preprocessing.bads import _find_outliers
 from psutil import virtual_memory
-from scipy.stats import iqr
+from scipy.stats import iqr, zscore
 from statsmodels.robust.scale import mad
+
+
+def _find_outliers(X, threshold=3.0, max_iter=2):
+    """Find outliers."""
+    my_mask = np.zeros(len(X), dtype=np.bool)
+    for _ in range(max_iter):
+        X = np.ma.masked_array(X, my_mask)
+        this_z = np.abs(zscore(X))
+        local_bad = this_z > threshold
+        my_mask = np.max([my_mask, local_bad], 0)
+        if not np.any(local_bad):
+            break
+
+    bad_idx = np.where(my_mask)[0]
+    return bad_idx
 
 
 def find_bad_epochs(epochs, picks=None, thresh=3.29053):
