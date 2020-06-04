3e27fd0be5faca8384306fd8d6f1fd521d9bee33
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-06-04 12:52:58 +0200

    solve E402 by ordering imports

--- pyprep/__init__.py
+++ pyprep/__init__.py
@@ -1,8 +1,8 @@
 """initialize pyprep."""
+from pyprep.noisy import find_bad_epochs, Noisydata  # noqa: F401
+from pyprep.prep_pipeline import PrepPipeline  # noqa: F401
+
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
-
-from pyprep.noisy import find_bad_epochs, Noisydata  # noqa: F401
-from pyprep.prep_pipeline import PrepPipeline  # noqa: F401
