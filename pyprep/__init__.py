954a03b193833d5f0c40d8586d1d4f5e7c2cbe9c
Austin Hurst <mynameisaustinhurst@gmail.com>
2021-04-24 18:04:13 -0300

    Add Reference to API so sphinxdoc can find it

--- pyprep/__init__.py
+++ pyprep/__init__.py
@@ -1,6 +1,7 @@
 """initialize pyprep."""
 import pyprep.ransac as ransac  # noqa: F401
 from pyprep.find_noisy_channels import NoisyChannels  # noqa: F401
+from pyprep.reference import Reference  # noqa: F401
 from pyprep.prep_pipeline import PrepPipeline  # noqa: F401
 
 from ._version import get_versions
