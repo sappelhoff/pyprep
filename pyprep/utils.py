707af5c4427752f9e5826861834dc895fbdd5cbf
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-03-21 22:57:41 +0100

    revert

--- pyprep/utils.py
+++ pyprep/utils.py
@@ -6,7 +6,6 @@ import mne
 import numpy as np
 import scipy.interpolate
 from mne.surface import _normalize_vectors
-from mne.utils import logger
 from numpy.polynomial.legendre import legval
 from psutil import virtual_memory
 from scipy import linalg
@@ -567,7 +566,7 @@ def _split_list(mylist, chunk_size):
 
 
 def _print_progress(current, end, start=None, stepsize=1, every=0.1):
-    """Log the current progress in a loop.
+    """Print the current progress in a loop.
 
     Parameters
     ----------
@@ -583,9 +582,9 @@ def _print_progress(current, end, start=None, stepsize=1, every=0.1):
         The fixed amount by which `current` increases every iteration of the
         loop. Defaults to ``1``.
     every: float, optional
-        The frequency with which to log progress updates during the loop,
+        The frequency with which to print progress updates during the loop,
         as a proportion between 0 and 1, exclusive. Defaults to ``0.1``, which
-        log a progress update after every 10%.
+        prints  a progress update after every 10%.
 
     """
     start = stepsize if not start else start
@@ -593,17 +592,15 @@ def _print_progress(current, end, start=None, stepsize=1, every=0.1):
     current = current - start + 1
 
     if current == 1:
-        logger.handlers[0].flush()
-        logger.info("Progress:")
+        print("Progress:", end=" ", flush=True)
     elif current == end:
-        logger.info("100%")
+        print("100%")
     elif current > 0:
         progress = float(current) / end
         last = float(current - stepsize) / end
         if int(progress / every) > int(last / every):
             pct = int(progress / every) * every * 100
-            logger.handlers[0].flush()
-            logger.info("{0}%...".format(int(pct)))
+            print("{0}%...".format(int(pct)), end=" ", flush=True)
 
 
 def _verify_free_ram(data, n_samples, n_channels, max_prop=0.95):
