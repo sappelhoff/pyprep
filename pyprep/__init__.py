ec51f7524d8c91ab5984a32db697e55b7340dc91
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2024-08-25 11:38:12 +0200

    MAINT: move to pyproject, track version with hatch vcs

--- pyprep/__init__.py
+++ pyprep/__init__.py
@@ -1,7 +1,12 @@
-"""initialize pyprep."""
+"""Initialize PyPREP."""
 import pyprep.ransac as ransac  # noqa: F401
 from pyprep.find_noisy_channels import NoisyChannels  # noqa: F401
 from pyprep.prep_pipeline import PrepPipeline  # noqa: F401
 from pyprep.reference import Reference  # noqa: F401
 
-__version__ = "0.5.0.dev0"
+try:
+    from importlib.metadata import version
+
+    __version__ = version("pyprep")
+except Exception:
+    __version__ = "0.0.0"
